747301f2505a235d1866087d1d838da8b92301a5
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-04-29 23:23:05 +0200

    last

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+let mixedVar: string | number;
+mixedVar = 'Hello';
+console.log(mixedVar);
+mixedVar = 123;
+console.log(mixedVar);
+
+let status: 'enable' | 'disable';
+status = 'enable';
+console.log(status);
